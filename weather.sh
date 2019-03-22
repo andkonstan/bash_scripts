4e4ea80bd5bc22375e31f63f7a7da05b5085739d
Andreas Konstantinidis <andrkonstant@gmail.com>
2019-03-22 22:08:28 +0200

    new weather mp3 url

--- weather.sh
+++ weather.sh
@@ -3,8 +3,8 @@
 date > date.txt
 espeak "the date is:" -g 5 -s 120
 cat date.txt |espeak -g 5 -s 120
-wget -q http://penteli.meteo.gr/innoetics/31.mp3 #replace 31 with id of city
-espeak "weather forecast for university, janina begins" -g 5 -s 120
+wget -q http://penteli.meteo.gr/stations/innoetics/31.mp3 #replace 31 with id of city
+espeak "weather forecast for janina begins" -g 5 -s 120
 mpg123 -q 31.mp3 #replace 31 with id of city
 rm 31.mp3 #replace 31 with id of city
 rm date.txt
